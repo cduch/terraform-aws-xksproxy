a43ea0f27ab0cddf008c4e3909b3c33796b88b25
Carsten Duch <carsten@hashicorp.com>
2023-02-02 17:53:31 +0100

    next try

--- config.sh
+++ config.sh
@@ -1,6 +1,9 @@
 
+export VAULT_ADDR=https://cduch-vault-01.hashidemos.io:8200/
+export VAULT_IP="54.195.130.133"
+
 # prepare system
-sudo yum install unzip git rpm-build gcc -y
+sudo yum install unzip git rpm-build gcc opensc -y
 
 
 # install and build xks-proxy
@@ -13,9 +16,22 @@ sudo yum install -y /home/centos/rpmbuild/RPMS/x86_64/xks-proxy-3.1.2-0.el7.x86_
 
 # install and configure libpkcs-11
 sudo mkdir /etc/kmip/
+scp ubuntu@$VAULT_ADDR:/tmp/*.pem /etc/kmip/
 curl https://releases.hashicorp.com/vault-pkcs11-provider/0.1.3/vault-pkcs11-provider_0.1.3_linux-el7_amd64.zip -o /tmp/vault-pkcs11-provider.zip
 unzip /tmp/vault-pkcs11-provider.zip -d /usr/local/lib
 
+sudo cat <<EOT >> /tmp/vault-pkcs11.hcl
+slot {
+  server = "$VAULT_IP:5696"
+  tls_cert_path = "/etc/kmip/cert.pem"
+  ca_path = "/etc/kmip/ca.pem"
+  scope = "my-service"
+}
+EOT
+sudo mv /tmp/vault-pkcs11.hcl /etc/
+
+sudo mkdir -p /var/local/xks-proxy/.secret/
+sudo cp /tmp/settings.toml /var/local/xks-proxy/.secret/
 
 # install and configure ngrok
 cd /tmp
